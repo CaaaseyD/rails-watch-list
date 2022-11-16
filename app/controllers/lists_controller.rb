91a3b6d736a31de001435248936a9f8e7740076d
Caaasey <baimaxsolution@gmail.com>
2022-11-16 16:56:08 +1100

    not pretty but working

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    bookmark = Bookmark.where(list_id: params[:id])
+    movie_id = bookmark.map { |m| m.movie_id }
+    @movies = movie_id.map { |id| Movie.find(id) }
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
