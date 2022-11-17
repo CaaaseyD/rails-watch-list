24afd813b9f2bd7d8bf534715e84125cce1b30dc
Caaasey <baimaxsolution@gmail.com>
2022-11-17 11:38:38 +1100

    update

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,14 +12,20 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
+    @list = List.find(@bookmark.list_id)
     if @bookmark.save
       redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark=Bookmark.find(params[:id])
+    @list = List.find(@bookmark.list_id)
+    @bookmark.destroy
+    redirect_to list_path(@list), notice: "Bookmark comment was successfully destroyed."
   end
 
   private
