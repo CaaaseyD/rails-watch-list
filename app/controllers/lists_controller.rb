7c9e0e8097878e597923df37bdc997270bf6f0c6
Caaasey <baimaxsolution@gmail.com>
2022-11-17 17:05:41 +1100

    add edit&delete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,6 +14,25 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to @list, notice: "List was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_url, notice: "List was successfully destroyed."
+  end
+
   def create
     @list = List.new(list_params)
     if @list.save
